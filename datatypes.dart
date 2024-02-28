9c3e4b28ff3552e28b72abf5077047324674836f
pavithramoorthy-official <pavithramoorthy.m@gmail.com>
2024-02-28 11:03:11 +0530

    print differt datatypes

---
+++ datatypes.dart
@@ -0,0 +1,24 @@
+void main() {
+  int a = 10;
+  double b = 145.876;
+  String c = "Hello, world";
+  bool d = true;
+  List e = [10, 20, 30, 40, 50, 10, 30, 20];
+  Set<int> f = {6, 5, 7, 8, 4};
+  var g = <String>{'a', 'b', 'c', 'd'};
+  var h = {'a': 'apple', 'b': 'bear', 'c': 'cat'};
+  var i = new Map();
+  i['1'] = 'one';
+  i['2'] = 'two';
+  i['3'] = 'three';
+
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+  print(f);
+  print(g);
+  print(h);
+  print(i);
+}
