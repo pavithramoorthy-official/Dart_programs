de710296762a815814574b5096d6adfbea8b2b4b
pavithramoorthy-official <pavithramoorthy.m@gmail.com>
2024-02-28 11:19:19 +0530

    updated variable name

--- datatypes.dart
+++ datatypes.dart
@@ -1,24 +1,42 @@
 void main() {
-  int a = 10;
-  double b = 145.876;
-  String c = "Hello, world";
+  int number = 10;
+  double percentage = 87.6;
+  String name = "Sita Rama";
   bool d = true;
-  List e = [10, 20, 30, 40, 50, 10, 30, 20];
-  Set<int> f = {6, 5, 7, 8, 4};
-  var g = <String>{'a', 'b', 'c', 'd'};
-  var h = {'a': 'apple', 'b': 'bear', 'c': 'cat'};
-  var i = new Map();
-  i['1'] = 'one';
-  i['2'] = 'two';
-  i['3'] = 'three';
+  List marks = [
+    95,
+    100,
+    80,
+    90,
+    90,
+  ];
+  Set<int> even_numbers = {2, 4, 6, 8, 10};
+  var alphabet = <String>{'a', 'b', 'c', 'd'};
+  var state_capital = {
+    'tamilnadu': 'chennai',
+    'manipur': 'imphal',
+    'telangana': 'hyderabad'
+  };
+  var number_to_word = new Map();
+  number_to_word['1'] = 'one';
+  number_to_word['2'] = 'two';
+  number_to_word['3'] = 'three';
 
-  print(a);
-  print(b);
-  print(c);
+  dynamic word = "Earth";
+  const k = "Super";
+  const int k1 = 100;
+  print(number);
+  print(percentage);
+  print(name);
   print(d);
-  print(e);
-  print(f);
-  print(g);
-  print(h);
-  print(i);
+  print(marks);
+  print(even_numbers);
+  print(alphabet);
+  print(state_capital);
+  print(number_to_word);
+  print(word);
+  word = 123;
+  print(word);
+  print(k);
+  print(k1);
 }
