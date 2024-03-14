a68ef2ce113acc7dcdfc2e5251acee54f92b0902
pavithramoorthy-official <pavithramoorthy.m@gmail.com>
2024-03-14 15:07:20 +0530

    optional parameter []

--- functions.dart
+++ functions.dart
@@ -1,22 +1,23 @@
 void main() {
   positional_param(5, 6);
   optional_param(9);
+  optional_param(10, 20);
   named_param(10, width: 30, height: 20);
   required_param(50, b: 60, c: 70);
 }
 
 void positional_param(int a, int b) {
-  print("$a, $b");
+  print("positional_param : $a, $b");
 }
 
-void optional_param(int a, {b = 10}) {
-  print("$a, $b");
+void optional_param(int a, [b = 10]) {
+  print("optional_param : $a, $b");
 }
 
 void named_param(length, {height, width}) {
-  print("$length, $height, $width");
+  print("named_param : $length, $height, $width");
 }
 
 void required_param(int a, {required b, required c}) {
-  print("$a, $b, $c");
+  print("required_param :$a, $b, $c");
 }
